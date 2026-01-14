aabf63107576191573b263954f4812b39705e707
jdguerrero <guerrero.johndm@gmail.com>
2026-01-15 05:34:48 +0800

    fix creation if table

--- db/cable_migrate/20240101000001_create_solid_cable_messages.rb
+++ db/cable_migrate/20240101000001_create_solid_cable_messages.rb
@@ -1,6 +1,6 @@
 class CreateSolidCableMessages < ActiveRecord::Migration[8.0]
   def change
-    create_table :solid_cable_messages do |t|
+    create_table :solid_cable_messages, if_not_exists: true do |t|
       t.binary :channel, null: false
       t.binary :payload, null: false
       t.datetime :created_at, null: false
